23286746724f405d01f8213eb8f3fd77e8055cf8
Neng <zhougn@gmail.com>
2016-09-30 13:38:31 +0800

    make it thread safe

--- lib/seventeen_mon/ipdb.rb
+++ lib/seventeen_mon/ipdb.rb
@@ -1,25 +1,16 @@
 module SeventeenMon
   class IPDB
+    attr_reader :offset, :index, :max_comp_length
 
     private_class_method :new
 
-    def ip_db_path
-      @ip_db_path ||= File.expand_path'../../data/17monipdb.dat', __FILE__
-    end
-    def ip_db
-      @ip_db ||= File.open ip_db_path, 'rb'
-    end
-
-    def offset
-      @offset ||= ip_db.read(4).unpack("Nlen")[0]
-    end
-
-    def index
-      @index ||= ip_db.read(offset - 4)
-    end
-
-    def max_comp_length
-      @max_comp_length ||= offset - 1028
+    def initialize
+      @ip_db_path = File.expand_path('../../data/17monipdb.dat', __FILE__)
+      File.open @ip_db_path, 'rb' do |ip_db|
+        @offset = ip_db.read(4).unpack("Nlen")[0]
+        @index = ip_db.read(offset - 4)
+        @max_comp_length = @offset - 1028
+      end
     end
 
     def self.instance
@@ -27,7 +18,7 @@ module SeventeenMon
     end
 
     def seek(_offset, length)
-      IO.read(ip_db_path, length, offset + _offset - 1024).split "\t"
+      IO.read(@ip_db_path, length, offset + _offset - 1024).split "\t"
     end
   end
 end
